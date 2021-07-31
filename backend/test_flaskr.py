b708a4ab1a9f6b7c691c4e3845575f1493091901
Yan Xu <xudaiyanzi@gmail.com>
2021-07-30 23:45:01 -0500

    add test_flaskr handlers, no error handlers

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -38,6 +38,37 @@ class BookTestCase(unittest.TestCase):
 #        You can feel free to write additional tests for nuanced functionality,
 #        Such as adding a book without a rating, etc. 
 #        Since there are four routes currently, you should have at least eight tests. 
+    def test_get_books(self):
+        """Test API can get a book (GET request)."""
+        res = self.client().get('/books')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_a_book(self):
+        """Test API can get a single book (GET request)."""
+        res = self.client().get('/books/2')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_book_not_found(self):
+        """Test API can get a single book (GET request)."""
+        res = self.client().get('/books/0')
+        self.assertEqual(res.status_code, 404)
+
+    def test_post_book(self):
+        """Test API can post a book (POST request)."""
+        res = self.client().post('/books', data=self.new_book)
+        self.assertEqual(res.status_code, 201)
+
+
+    def test_delete_book(self):
+        """Test API can delete a book (DELETE request)."""
+        res = self.client().delete('/books/2')
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_book_not_found(self):
+        """Test API can delete a book (DELETE request)."""
+        res = self.client().delete('/books/0')
+        self.assertEqual(res.status_code, 404)
+
 # Optional: Update the book information in setUp to make the test database your own! 
 
 
