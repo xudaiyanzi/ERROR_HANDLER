e4960743d771fed104aa9919ba24d2a01c410887
Yan Xu <xudaiyanzi@gmail.com>
2021-07-31 01:08:22 -0500

    use self.assertTrue not self.assertEqual

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -23,12 +23,12 @@ class BookTestCase(unittest.TestCase):
             'rating': 5
         }
 
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
+        # # binds the app to the current context
+        # with self.app.app_context():
+        #     self.db = SQLAlchemy()
+        #     self.db.init_app(self.app)
+        #     # create all tables
+        #     self.db.create_all()
     
     def tearDown(self):
         """Executed after reach test"""
@@ -38,36 +38,52 @@ class BookTestCase(unittest.TestCase):
 #        You can feel free to write additional tests for nuanced functionality,
 #        Such as adding a book without a rating, etc. 
 #        Since there are four routes currently, you should have at least eight tests. 
-    def test_get_books(self):
-        """Test API can get a book (GET request)."""
+    ### I need to add data to the table first
+    ### to do this, I use the command line below
+
+    ## $psql -f books.psql -U student -d bookshelf_test
+
+    def test_pagination(self):
+        """Test the pagination"""
         res = self.client().get('/books')
-        self.assertEqual(res.status_code, 200)
+        data = json.loads(res.data)
 
-    def test_get_a_book(self):
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        ### be careful!!!! self.assertTrue VS self.assertEqual
+        self.assertTrue(data['total_books'])
+        self.assertTrue(len(data['books']))
+
+    def test_error_404_get_book_not_found(self):
         """Test API can get a single book (GET request)."""
-        res = self.client().get('/books/2')
-        self.assertEqual(res.status_code, 200)
+        res = self.client().get('/books?page=10')
+        data = json.loads(res.data)
 
-    def test_get_book_not_found(self):
-        """Test API can get a single book (GET request)."""
-        res = self.client().get('/books/0')
         self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['sucess'], False)
+        self.assertEqual(data['message'], 'not found')
 
-    def test_post_book(self):
-        """Test API can post a book (POST request)."""
-        res = self.client().post('/books', data=self.new_book)
-        self.assertEqual(res.status_code, 201)
+    # def test_get_a_book(self):
+    #     """Test API can get a single book (GET request)."""
+    #     res = self.client().get('/books/3')
+    #     self.assertEqual(res.status_code, 200)
 
 
-    def test_delete_book(self):
-        """Test API can delete a book (DELETE request)."""
-        res = self.client().delete('/books/2')
-        self.assertEqual(res.status_code, 200)
+    # def test_post_book(self):
+    #     """Test API can post a book (POST request)."""
+    #     res = self.client().post('/books', data=self.new_book)
+    #     self.assertEqual(res.status_code, 201)
 
-    def test_delete_book_not_found(self):
-        """Test API can delete a book (DELETE request)."""
-        res = self.client().delete('/books/0')
-        self.assertEqual(res.status_code, 404)
+
+    # def test_delete_book(self):
+    #     """Test API can delete a book (DELETE request)."""
+    #     res = self.client().delete('/books/2')
+    #     self.assertEqual(res.status_code, 200)
+
+    # def test_delete_book_not_found(self):
+    #     """Test API can delete a book (DELETE request)."""
+    #     res = self.client().delete('/books/0')
+    #     self.assertEqual(res.status_code, 422)
 
 # Optional: Update the book information in setUp to make the test database your own! 
 
